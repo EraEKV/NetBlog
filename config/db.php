776715e874da7a901d43bff1d78beb1aad4b165d
Erasyl <112917290+EraEKV@users.noreply.github.com>
2025-02-05 00:01:43 +0500

    Update db.php
    test

--- config/db.php
+++ config/db.php
@@ -6,7 +6,7 @@
     date_default_timezone_set('Asia/Almaty');
 
     // $con - это само подключение к БД(базе данных)
-    $con = mysqli_connect("db-mysql-blr1-28936-do-user-17259326-0.k.db.ondigitalocean.com:25060", "doadmin", "AVNS_cGBV2-u6mv1pdMBua3N", "defaultdb" );     // blognet - Этто наше БД
+    $con = mysqli_connect("db-mysql-blr1-28936-do-user-17259326-0.k.db.ondigitalocean.com", "doadmin", "AVNS_cGBV2-u6mv1pdMBua3N", "defaultdb", 25060);     // blognet - Этто наше БД
     mysqli_set_charset($con, "utf8");
 
     if(mysqli_connect_errno()) {
